773fd19e2aad68f193e3b2ca24c29f83e8a8cd74
Axel Dalí Gómez <a329881@uach.mx>
2022-11-22 16:29:24 -0600

    Se cambia el funcionamiento de las predicciones

--- functions/func.js
+++ functions/func.js
@@ -1,70 +1,72 @@
 'use strict';
 
 async function main(text) {
-  // [START aiplatform_predict_text_classification_sample]
-  /**
-   * TODO(developer): Uncomment these variables before running the sample.\
-   * (Not necessary if passing values as arguments)
-   */
+  return new Promise(res => {
+    // [START aiplatform_predict_text_classification_sample]
+    /**
+     * TODO(developer): Uncomment these variables before running the sample.\
+     * (Not necessary if passing values as arguments)
+     */
 
-  // const text = 'YOUR_PREDICTION_TEXT';
-  const endpointId = '7189086409469198336';
-  const project = '339726620004';
-  const location = 'us-central1';
-  const aiplatform = require('@google-cloud/aiplatform');
-  const {instance, prediction} =
-    aiplatform.protos.google.cloud.aiplatform.v1.schema.predict;
+    // const text = 'YOUR_PREDICTION_TEXT';
+    const endpointId = '7189086409469198336';
+    const project = '339726620004';
+    const location = 'us-central1';
+    const aiplatform = require('@google-cloud/aiplatform');
+    const {instance, prediction} =
+      aiplatform.protos.google.cloud.aiplatform.v1.schema.predict;
 
-  // Imports the Google Cloud Model Service Client library
-  const {PredictionServiceClient} = aiplatform.v1;
+    // Imports the Google Cloud Model Service Client library
+    const {PredictionServiceClient} = aiplatform.v1;
 
-  // Specifies the location of the api endpoint
-  const clientOptions = {
-    apiEndpoint: 'us-central1-aiplatform.googleapis.com',
-  };
-
-  // Instantiates a client
-  const predictionServiceClient = new PredictionServiceClient(clientOptions);
-
-  async function predictTextClassification() {
-    // Configure the resources
-    const endpoint = `projects/${project}/locations/${location}/endpoints/${endpointId}`;
-
-    const predictionInstance =
-      new instance.TextClassificationPredictionInstance({
-        content: text,
-      });
-    const instanceValue = predictionInstance.toValue();
-
-    const instances = [instanceValue];
-    const request = {
-      endpoint,
-      instances,
+    // Specifies the location of the api endpoint
+    const clientOptions = {
+      apiEndpoint: 'us-central1-aiplatform.googleapis.com',
     };
 
-    const [response] = await predictionServiceClient.predict(request);
-    console.log('Predict text classification response');
-    console.log(`\tDeployed model id : ${response.deployedModelId}\n\n`);
+    // Instantiates a client
+    const predictionServiceClient = new PredictionServiceClient(clientOptions);
 
-    console.log('Prediction results:');
+    async function predictTextClassification() {
+      // Configure the resources
+      const endpoint = `projects/${project}/locations/${location}/endpoints/${endpointId}`;
 
-    for (const predictionResultValue of response.predictions) {
-      const predictionResult =
-        prediction.ClassificationPredictionResult.fromValue(
-          predictionResultValue
-        );
+      const predictionInstance =
+        new instance.TextClassificationPredictionInstance({
+          content: text,
+        });
+      const instanceValue = predictionInstance.toValue();
 
-      for (const [i, label] of predictionResult.displayNames.entries()) {
-        console.log(`\tDisplay name: ${label}`);
-        console.log(`\tConfidences: ${predictionResult.confidences[i]}`);
-        console.log(`\tIDs: ${predictionResult.ids[i]}\n\n`);
+      const instances = [instanceValue];
+      const request = {
+        endpoint,
+        instances,
+      };
+
+      const [response] = await predictionServiceClient.predict(request);
+      console.log('Predict text classification response');
+      console.log(`\tDeployed model id : ${response.deployedModelId}\n\n`);
+
+      console.log('Prediction results:');
+
+      for (const predictionResultValue of response.predictions) {
+        const predictionResult =
+          prediction.ClassificationPredictionResult.fromValue(
+            predictionResultValue
+          );
+
+        for (const [i, label] of predictionResult.displayNames.entries()) {
+          console.log(`\tDisplay name: ${label}`);
+          console.log(`\tConfidences: ${predictionResult.confidences[i]}`);
+          console.log(`\tIDs: ${predictionResult.ids[i]}\n\n`);
+        }
       }
-    }
 
-    return await response
-  }
-  predictTextClassification();
-  // [END aiplatform_predict_text_classification_sample]
+      return response
+    }
+    res(predictTextClassification());
+    // [END aiplatform_predict_text_classification_sample]
+  })
 }
 
 process.on('unhandledRejection', err => {
--- server.js
+++ server.js
@@ -1,21 +1,38 @@
 const express = require('express');
 const path = require('path');
 const http = require('http');
-const prediction = require('./functions/func')
+const bodyParser = require('body-parser');
+const prediction = require('./functions/func');
+const { response } = require('express');
 
 const app = express();
 const port = process.env.PORT || 8080;
+
+app.use('/css', express.static(path.resolve(__dirname, 'css')))
+
+let urlencodedParser = bodyParser.urlencoded({ extended: false })
 
 // sendFile will go here
 app.get('/', function(req, res) {
   res.sendFile(path.join(__dirname, '/index.html'));
 });
 
-app.get('/:id', function(req, res) {
-  console.log(req.params['id']);
+app.post('/prediction', urlencodedParser, async (req, res, next) => {
+  let url = req.body.url
+  console.log(url)
+  let ans = await prediction.main(url);
+  console.log(ans)
   res.setHeader('Content-Type', 'application/json');
-  //res.end(JSON.stringify(prediction.main('google.com/mail/u/1/#inbox')));
-  res.end(JSON.stringify(prediction.main(decodeURIComponent(req.params['id']))));
+  res.end(JSON.stringify(ans));
+});
+
+app.get('/prediction/:id', async (req, res) => {
+  ///console.log(req.params['id']);
+
+  let ans = await prediction.main('google.com/mail/u/1/#inbox');
+  console.log(ans)
+  res.end(JSON.stringify(ans));
+  ///res.end(JSON.stringify(prediction.main(decodeURIComponent(req.params['id']))));
 });
 
 app.listen(port);
